e1544ea5dbb8842b445a5e93de446cb8b41a7311
pinostrel <pinostrel@gmail.com>
2019-12-23 20:19:50 +0100

    CRUD Operations Added

--- GuitarShop/GuitarShop/Shared/Models/Guitar.cs
+++ GuitarShop/GuitarShop/Shared/Models/Guitar.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace GuitarShop.Shared.Models
@@ -7,7 +8,9 @@ namespace GuitarShop.Shared.Models
     public class Guitar
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public int Price { get; set; }
     }
 }
